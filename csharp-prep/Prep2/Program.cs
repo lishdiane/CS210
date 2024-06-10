7b415555de55c0289efa6308334380ae136f74f1
lishdiane <meafunkymunky@yahoo.com>
2024-06-10 15:01:17 -0600

    Grade code added

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,60 @@
 using System;
+using System.Net.NetworkInformation;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What grade percentage did you receive? ");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+
+        int lastDigit = number % 10;
+
+        string letter = "";
+        string suffix = "";
+
+
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else if (number < 60)
+        {
+            letter = "F";
+        }
+
+        if (lastDigit >= 7 && letter != "A" && letter != "F")
+        {
+            suffix = "+";
+        }
+        else if (lastDigit < 3 && letter != "F")
+        {
+            suffix = "-";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}{suffix}");
+
+        if (number >= 70)
+        {
+            Console.WriteLine("You passed the class!");
+        }
+        else 
+        {
+            Console.WriteLine("You did not pass the class.");
+        }
+
     }
 }
