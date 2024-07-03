ecec9b5cdaa2705d0bf4fdc777b2a0f3fc724a9f
lishdiane <meafunkymunky@yahoo.com>
2024-07-03 15:45:24 -0600

    Added code for Learning Activity 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Diane Lish", "Spelling");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Tyler Lish", "Equations", "7.3", "3-10");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomework());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Eva Lish", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInfo());
     }
 }
